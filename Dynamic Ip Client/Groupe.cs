c60be9c003e79a13e051ae6fa992fa7723cbcb1f
guldmann <john@pcis.se>
2016-10-18 23:39:42 +0200

    Added groups object to hold domain names hosted at one provider

---
+++ Dynamic Ip Client/Groupe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Ip_Client
+{
+    class Groupe
+    {
+        public string Name { get; set; }
+        private List<string>  domains { get; }= new List<string>();
+        public DNSProvider DnsProvider { get; set; }
+        public  string Username { get; set; }
+        public  string Password { get; set; }
+        public void AddDomain(string domain)
+        {
+            domains.Add(domain);
+        }
+    }
+}
